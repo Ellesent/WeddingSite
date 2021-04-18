0180f6270898025adbf679621d357f9a64bf4a49
ellesent <hypahflipperz@gmail.com>
2021-04-17 20:49:20 -0700

    working on firebase authentication for admins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import 'tailwindcss/tailwind.css'
 import { Layout } from '../components/layout'
 import {NavigationBar} from '../components/navigation-bar'
+import firebase from "firebase/app";
+import "firebase/auth";
+import { FirebaseAuthProvider } from '@react-firebase/auth';
+import {firebaseConfig} from "../firebase-config"
 
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
+    <FirebaseAuthProvider  firebase={firebase} {...firebaseConfig}>
   <NavigationBar></NavigationBar>
   <Component {...pageProps} />
+  </FirebaseAuthProvider>
   </Layout>
   )
 }
