fe63ea1e13197057f1dbf9bd59788871d3141973
ellesent <hypahflipperz@gmail.com>
2021-05-08 20:10:19 -0700

    styling RSVP page

--- pages/rsvp/[id].tsx
+++ pages/rsvp/[id].tsx
@@ -4,7 +4,7 @@ import { Guest } from '../../utils/Types';
 
 const RSVPUnique = () => {
     const router = useRouter();
-    
+
     const { id } = router.query;
 
     const [guestInfo, setGuestInfo] = useState<Guest>();
@@ -15,43 +15,42 @@ const RSVPUnique = () => {
     useEffect(() => {
         const fetchGuest = async () => {
             try {
-            const response = await fetch(`/api/guests/${id}`, { method: 'GET' })
+                const response = await fetch(`/api/guests/${id}`, { method: 'GET' })
 
-            // check if guest was found
-            if (response.status == 400) {
-                console.error("guest not found!")
-                setGuestInfo(null);
+                // check if guest was found
+                if (response.status == 400) {
+                    console.error("guest not found!")
+                    setGuestInfo(null);
+                }
+
+                const guestInfoJSON = await response.json();
+                console.log(guestInfoJSON);
+                setGuestInfo(guestInfoJSON);
             }
-
-            const guestInfoJSON = await response.json();
-            console.log(guestInfoJSON);
-            setGuestInfo(guestInfoJSON);
-        }
-        catch (ex) {
-            console.error(`something went wrong fetching ${ex}`)
-        }
+            catch (ex) {
+                console.error(`something went wrong fetching ${ex}`)
+            }
 
         }
         if (id) {
-        fetchGuest();
+            fetchGuest();
         }
     }, [id])
 
     return (
-        <div>
+        <div className="flex justify-center items-center m-5">
             {guestInfo == undefined ?
                 <p>Guest information not found! Please make sure the URL is correct.</p>
                 :
                 // TODO check if the guest has RSVPd already and let them know
-                <div>
+                <div className="flex flex-col items-center justify-center m-5">
                     <p>Found your guest information! Please make sure the information below is correct before RSVPing. If any of this looks incorrect please contact the Bride and Groom.</p>
-                    <div>
-                        <p>{guestInfo.name}</p>
-                        <p>{guestInfo.numInParty}</p>
-                        <p>{guestInfo.email}</p>
-                        <p>{guestInfo.address}</p>
-                        <p>{guestInfo.foodAllergies}</p>
-                    </div>
+                    <p className="mt-5">{`Name: ${guestInfo.name}`}</p>
+                    <p>{`Number in Party: ${guestInfo.numInParty}`}</p>
+                    <p>{`Email: ${guestInfo.email}`}</p>
+                    <p>{`Address: ${guestInfo.address}`}</p>
+                    <p>{`Food Allergies: ${guestInfo.foodAllergies}`}</p>
+                    <button className="m-5 p-2 border-2 rounded bg-yellow-500">Click HERE to RSVP</button>
                 </div>}
         </div>
     )
