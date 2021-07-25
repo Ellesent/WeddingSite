11fd214a2494ff15a13eb917e8f018ff9ef3fd00
ellesent <hypahflipperz@gmail.com>
2021-07-24 18:40:59 -0700

    adding cloudbuild yaml due to needing secret env variables for the build

--- pages/venue.tsx
+++ pages/venue.tsx
@@ -1,5 +1,4 @@
 import { Maps } from "../components/maps"
-import config from "../config.json"
 
 const Venue = () => {
 
@@ -9,7 +8,7 @@ const Venue = () => {
             <p className="pb-5">Address: 9565 Semiahmoo Pkwy Blaine, WA 98230</p>
             <Maps
                 isMarkerShown
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${config["google-api-key"]}&libraries=geometry,drawing,places`}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=geometry,drawing,places`}
                 loadingElement={<div className="flex h-1/2" />}
                 containerElement={<div className="flex h-96" />}
                 mapElement={<div className="flex-auto" />}
