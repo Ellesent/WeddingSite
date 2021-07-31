1bcb90b8db8e63d9ee6be88807b21fb453f6faa4
ellesent <hypahflipperz@gmail.com>
2021-07-31 16:58:04 -0700

    shutting down the RSVP page for now

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -1,11 +1,12 @@
 const RSVP = () => {
     return (
         <div className="flex flex-col items-center p-20">
-            <p className="font-serif text-4xl flex flex-col">
+            <h1 className="text-4xl">You cannot RSVP yet. Look out for an invitation with an RSVP URL code in the future!</h1>
+            {/* <p className="font-serif text-4xl flex flex-col">
                 IN ORDER TO RSVP, INPUT THE URL WRITTEN ON YOUR INVITATION
             <br />
                 <p className="text-base self-center">PLEASE REACH OUT TO THE COUPLE If YOU NEED HELP FINDING THE URL</p>
-            </p>
+            </p> */}
         </div>
     )
 }
