833d953103ccabfd94cce1876a8752751af79f72
ellesent <hypahflipperz@gmail.com>
2021-08-01 11:47:58 -0700

    fixing google map issue + trying to fix auto cloud run issue

--- pages/venue.tsx
+++ pages/venue.tsx
@@ -1,4 +1,5 @@
 import { Maps } from "../components/maps"
+import config from "../config.json"
 
 const Venue = () => {
 
@@ -6,9 +7,10 @@ const Venue = () => {
         <div className="m-5">
             <p>Name: The Edgewater House</p>
             <p className="pb-5">Address: 11967 Luna Vista Ave SE, Olalla, WA 98359</p>
+            <p>{process.env.REACT_APP_GOOGLE_MAPS_API_KEY}</p>
             <Maps
                 isMarkerShown
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=geometry,drawing,places`}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${config["google-api-key"]}&libraries=geometry,drawing,places`}
                 loadingElement={<div className="flex h-1/2" />}
                 containerElement={<div className="flex h-96" />}
                 mapElement={<div className="flex-auto" />}
