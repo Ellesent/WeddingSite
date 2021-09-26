b4a9ec51735203675076f0965aea16a452a12de9
ellesent <hypahflipperz@gmail.com>
2021-09-26 12:49:28 -0700

    copying _app.tsx from frankie branch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import 'tailwindcss/tailwind.css'
-import { Layout } from '../components/Layout'
+import { Layout } from '@components/Layout'
 import {NavigationBar} from '../components/navigation-bar'
-import firebase from "firebase/app";
-import "firebase/auth";
-import { FirebaseAuthProvider } from '@react-firebase/auth';
+import firebase from "firebase/app"
+import "firebase/auth"
+import { FirebaseAuthProvider } from '@react-firebase/auth'
 import {firebaseConfig} from "../firebase-config"
+
+import '../styles/styles.css'
 
 function MyApp({ Component, pageProps }) {
   return (
