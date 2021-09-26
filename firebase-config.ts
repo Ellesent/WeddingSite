6a36f0e948d1ef091147d8225044aad09c9cba08
ellesent <hypahflipperz@gmail.com>
2021-09-26 13:26:47 -0700

    email tested - switching over to Casey's db

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,15 +1,15 @@
 // Firebase Config
 
 // test project
-const firebaseConfig = {
-  apiKey: "AIzaSyB1A8fCA2IyAp27kI52Jq6Ch33-97JkO3Q",
-  authDomain: "wedding-site-test-74c21.firebaseapp.com",
-  projectId: "wedding-site-test-74c21",
-  storageBucket: "wedding-site-test-74c21.appspot.com",
-  messagingSenderId: "41821028712",
-  appId: "1:41821028712:web:beb1c304be4ad58cd316dd",
-  measurementId: "G-2889RWKYB7"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB1A8fCA2IyAp27kI52Jq6Ch33-97JkO3Q",
+//   authDomain: "wedding-site-test-74c21.firebaseapp.com",
+//   projectId: "wedding-site-test-74c21",
+//   storageBucket: "wedding-site-test-74c21.appspot.com",
+//   messagingSenderId: "41821028712",
+//   appId: "1:41821028712:web:beb1c304be4ad58cd316dd",
+//   measurementId: "G-2889RWKYB7"
+// };
 
 // our site
 // const firebaseConfig = {
@@ -24,15 +24,15 @@ const firebaseConfig = {
 
 
   // casey's site
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyDPy1Syrzj2qY7pCCOT3H5M51tlMeCvz_U",
-  //   authDomain: "casey-wedding-site.firebaseapp.com",
-  //   projectId: "casey-wedding-site",
-  //   storageBucket: "casey-wedding-site.appspot.com",
-  //   messagingSenderId: "557413758637",
-  //   appId: "1:557413758637:web:24e61c1ff1a663cdda3824",
-  //   databaseURL: ""
-  // };
+  const firebaseConfig = {
+    apiKey: "AIzaSyDPy1Syrzj2qY7pCCOT3H5M51tlMeCvz_U",
+    authDomain: "casey-wedding-site.firebaseapp.com",
+    projectId: "casey-wedding-site",
+    storageBucket: "casey-wedding-site.appspot.com",
+    messagingSenderId: "557413758637",
+    appId: "1:557413758637:web:24e61c1ff1a663cdda3824",
+    databaseURL: ""
+  };
   
 
   export {firebaseConfig}
