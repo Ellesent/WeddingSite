f97b8e11325fa4478709fff6d1213c4cd5c71cef
ellesent <hypahflipperz@gmail.com>
2021-11-13 14:15:57 -0800

    gift registry picture up

--- pages/giftregistry.tsx
+++ pages/giftregistry.tsx
@@ -1,7 +1,10 @@
+import styles from '../styles/giftregistry.module.css'
+
 const GiftRegistry = () => {
     return (
-        <div>
-
+        <div className='m-5 p-10 flex items-center flex-col'>
+            <p className={`${styles.text} text-4xl`}>Check Back Soon!</p>
+            <img className={`m-5 ${styles.img}`} src="/GiftRegistry.png" alt="presents drawing"/>
         </div>
     )
 }
