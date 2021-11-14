497b0e6d675340e94368f6713e2d726d2f0ea00b
ellesent <hypahflipperz@gmail.com>
2021-11-13 18:30:37 -0800

    home page hooray looks good

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="flex items-center flex-col gap-10 justify-center  h-full">
-     <p className="text-9xl transform-gpu -rotate-6">HOORAY!</p>
-     <p className={`${styles.subText} text-xl`}>{"Casey & Thomas are getting married on March 19th, 2022"}</p>
+    <div className="flex items-center flex-col gap-10 justify-center content-center h-full">
+     <img className="" src='WebsiteHomePageDesign.png'/>
+     <p className={`${styles.subText} text-xl leading-relaxed`}>{"Casey & Thomas are getting married on March 19th, 2022"}</p>
     </div>
   )
 }
