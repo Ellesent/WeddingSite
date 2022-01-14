69150d1117e84fcc8cab582598e9e77d7618021d
ellesent <hypahflipperz@gmail.com>
2022-01-14 15:03:09 -0800

    wording update

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -2,7 +2,7 @@ const RSVP = () => {
     return (
         <div className="flex flex-col items-center p-20">
             <p className="text-4xl flex flex-col">
-               Please check for an invitation in your email, and click the link there to RSVP! Reach out to the bride and groom for any questions.
+               Please check your email for an invitation and click the link in the email to RSVP! Reach out to the bride and groom for any questions.
             </p>
         </div>
     )
