ab743ea889c38612c50cc72ecbfee27ea8857483
ellesent <hypahflipperz@gmail.com>
2022-01-23 14:13:37 -0800

    added basic gift registry for now

--- pages/giftregistry.tsx
+++ pages/giftregistry.tsx
@@ -1,9 +1,17 @@
+import styles from "../styles/giftregistry.module.css";
+
 const GiftRegistry = () => {
-    return (
-        <div id='giftregistry'>
+  return (
+    <div className="m-5 p-10 flex items-center align-center justify-start flex-col h-full" id='giftregistry'>
+      <a
+        href="https://www.crateandbarrel.com/gift-registry/francesca-pate-rinaldi-and-dominic-valenciana/r6446476"
+        target="_blank"
+        className={`${styles.text} m-5 text-4xl underline lg:no-underline lg:hover:underline lg:transition lg:duration-500 lg:ease-in-out lg:transform lg:hover:-translate-y-1 lg:hover:scale-110`}
+      >
+        Crate and Barrel Gift Registry
+      </a>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default GiftRegistry
+export default GiftRegistry;
