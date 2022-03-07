45f50ce61f18e45aeabe8b001705ac2a01d99392
Ellesent <francesca.paterinaldi@gmail.com>
2022-03-06 18:10:30 -0800

    updating RSVP section

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -1,7 +1,7 @@
 const RSVP = () => {
     return (
         <div className="flex flex-col items-center p-20" id='rsvp'>
-            <h1 className="text-4xl">You cannot RSVP yet. Look out for an invitation with an RSVP URL code in the future!</h1>
+            <h1 className="text-4xl"> Please check your email for an invitation and click the link in the email to RSVP! Reach out to the bride and groom for any questions.</h1>
             {/* <p className="font-serif text-4xl flex flex-col">
                 IN ORDER TO RSVP, INPUT THE URL WRITTEN ON YOUR INVITATION
             <br />
