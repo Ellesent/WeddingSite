9c38bc77898a033b18b01a39432e8fd2dea8b235
ellesent <hypahflipperz@gmail.com>
2022-07-14 13:34:19 -0700

    added some parking info and some formatting

--- pages/venue.tsx
+++ pages/venue.tsx
@@ -4,11 +4,14 @@ import config from "../config.json"
 const Venue = () => {
 
     return (
-        <div className="m-5 flex flex-col" id='venue'>
+        <div className="m-5 flex flex-col text-center" id='venue'>
             <span className={`text-5xl m-5`}>-VENUE INFORMATION-</span>
-            <div className='flex flex-col justify-center content-center items-center justify-items-center justify-self-center'>
-            <p>Name: Cedar Springs</p>
-            <p className="pb-5">Address: 7354 Bethel Burley RD SE Port Orchard, WA 98367</p>
+            <div className='flex flex-col items-start'>
+            <p className="text-2xl font-bold">Cedar Springs</p>
+            <p className="">Address: 7354 Bethel Burley RD SE Port Orchard, WA 98367</p>
+            <p className="pb-5">Parking: The driveway is located on Bethel Burley RD it has large hedges on each side.
+Please proceed past the blue house to the bottom of the hill. <br/>Park near the Cedar Springs sign (large log with metal lettering). If you wind up on Lider Rd you have gone too far
+</p>
             </div>
             <Maps
                 isMarkerShown={true}
